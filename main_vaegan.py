d579c5a687c0a73c25802880c8944c50c8aba833
Youzhao Yang <computerscienceyyz@163.com>
2017-11-17 20:55:18 +0800

    Add files via upload

---
+++ main_vaegan.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+import os
+from VAE_GAN import *
+
+flags = tf.app.flags
+flags.DEFINE_bool("is_training", False, "training flag")
+FLAGS = flags.FLAGS
+
+def check_dir():
+    if not os.path.exists('./sample'):
+        os.mkdir('./sample')
+    if not os.path.exists('./checkpoint'):
+        os.mkdir('./checkpoint')
+    if not os.path.exists('./logs'):
+        os.mkdir('./logs')
+
+if __name__=='__main__':
+    check_dir()
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.33)
+    config = tf.ConfigProto(gpu_options=gpu_options)
+    config.gpu_options.allow_growth=True
+    with tf.Session(config=config) as sess:
+        vae_gan = VAE_GAN(input_height=64, input_width=64, input_channels=3, output_height=64, output_width=64, gf_dim=64, input_fname_pattern = '*.jpg', is_grayscale=False, sess = sess)
+        vae_gan.build_model()
+        if FLAGS.is_training:
+            vae_gan.train()
